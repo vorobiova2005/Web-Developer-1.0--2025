84b897568901ab59db3a58adb6fc41eb9c83cefc
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-04-09 11:45:39 +0300

    Add tabs with styles and script

--- Pulse/js/script.js
+++ Pulse/js/script.js
@@ -13,5 +13,23 @@ $(document).ready(function(){
                 }
             }
         ]
-      });  
+      });
+      $('ul.catalog-tabs').on('click', 'li:not(.catalog-tab-active)', function() {
+        $(this)
+          .addClass('catalog-tab-active').siblings().removeClass('catalog-tab-active')
+          .closest('div.container').find('div.catalog-content').removeClass('catalog-content-active').eq($(this).index()).addClass('catalog-content-active');
+    }); 
+    
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item-content').eq(i).toggleClass('catalog-item-content-active');
+                $('.catalog-item-list').eq(i).toggleClass('catalog-item-list-active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item-link');
+    toggleSlide('.catalog-item-back');
   });
