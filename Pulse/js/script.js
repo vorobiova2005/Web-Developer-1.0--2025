0076e0ccb3d3954543c3b17ba22484e30ff5e960
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-04-11 10:37:45 +0300

    Add smooth scroll and pageup

--- Pulse/js/script.js
+++ Pulse/js/script.js
@@ -97,4 +97,18 @@ $(document).ready(function(){
     });
     return false;
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else{
+      $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
 });
