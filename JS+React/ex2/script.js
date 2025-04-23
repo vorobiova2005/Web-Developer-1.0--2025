11a3d0d5c98ba2297a9029c034561be8ff6629fe
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-04-23 11:46:44 +0300

    Add practical

--- JS+React/ex2/script.js
+++ JS+React/ex2/script.js
@@ -1,8 +1,24 @@
 const usdCurr = 28;
-const eurCurr = 32;
+/* const eurCurr = 32; */
+const discount = 0.9;
 
 function convert (amount, curr){
-    console.log(curr * amount);
+    return curr * amount;
 }
-convert(500, usdCurr);
-convert(500, eurCurr);
+
+function promotion(result){
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+
+function test(){
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+        if(i === 3) return
+    }
+    console.log('Done');
+}
+test();
--- JS+React/practical_task/script.js
+++ JS+React/practical_task/script.js
@@ -1,4 +1,16 @@
-const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+'use strict';
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,35 +19,49 @@ const personalMovieDB = {
     privat: false
 };
 
-/* 
-Автоматизація коду за допомоою циклів
-const a = prompt('Один з переглянутих фільмів?', ''),
-      b = prompt('На скільки ви оціните цого?', ''),
-      c = prompt('Один з переглянутих фільмів?', ''),
-      d = prompt('На скільки ви оціните цого?', '');
-personalMovieDB.movies[c] = d; */
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Один з переглянутих фільмів?', ''),
+              b = prompt('На скільки ви оціните цого?', '');
+    
+        if (a != null && b != null && a !='' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else{
+            console.log('error');
+            i--;
+        }
+    } 
+}
 
-for (let i = 0; i < 2; i++){
-    const a = prompt('Один з переглянутих фільмів?', ''),
-          b = prompt('На скільки ви оціните цого?', '');
+rememberMyFilms();
 
-    if (a != null && b != null && a !='' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log('done');
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        console.log("Ви подиивилися доволі мало фільмів");
+    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Ви класичний глядач");
+    } else if(personalMovieDB.count >= 30) {
+        console.log("Ви кіноман");
     } else{
-        console.log('error');
-        i--;
+        console.log("Сталася помилка");
     }
 }
 
-if(personalMovieDB.count < 10){
-    console.log("Ви подиивилися доволі мало фільмів");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Ви класичний глядач");
-} else if(personalMovieDB.count >= 30) {
-    console.log("Ви кіноман");
-} else{
-    console.log("Сталася помилка");
+detectPersonalLevel();
+
+function showMyDB(hidden){
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenders(){
+    for (let i = 1; i <=3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
+    }
+}
+
+writeYourGenders();
