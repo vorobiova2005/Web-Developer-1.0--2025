61f1e05b3bf7e39031982ccc6caef3e41c991125
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-04-24 14:01:47 +0300

    Add pr

--- JS+React/practical_task/script.js
+++ JS+React/practical_task/script.js
@@ -1,67 +1,69 @@
 'use strict';
-let numberOfFilms;
-
-function start(){
-    numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '').trim();
-    }
-}
-
-start();
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors:{},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++){
-        const a = prompt('Один з переглянутих фільмів?', '').trim(),
-              b = prompt('На скільки ви оціните цого?', '').trim();
+    privat: false,
+    start: function(){
+        personalMovieDB.count = +prompt('Скільки фільмів ви вже переглянули?', '');
     
-        if (a != null && b != null && a !='' && b != '' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else{
-            console.log('error');
-            i--;
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Скільки фільмів ви вже переглянули?', '').trim();
         }
-    } 
-}
+    }, 
+    rememberMyFilms: function(){
+        for (let i = 0; i < 2; i++){
+            const a = prompt('Один з переглянутих фільмів?', '').trim(),
+                  b = prompt('На скільки ви оціните цого?', '').trim();
+        
+            if (a != null && b != null && a !='' && b != '' && a.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else{
+                console.log('error');
+                i--;
+            }
+        } 
+    },
+    detectPersonalLevel: function(){
+        if(personalMovieDB.count < 10){
+            console.log("Ви подиивилися доволі мало фільмів");
+        } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Ви класичний глядач");
+        } else if(personalMovieDB.count >= 30) {
+            console.log("Ви кіноман");
+        } else{
+            console.log("Сталася помилка");
+        }
+    },
+    showMyDB: function(hidden){
+        if(!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        } else{
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenders: function(){
+        for (let i = 1; i <=3; i++){
+            personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
 
-rememberMyFilms();
-
-function detectPersonalLevel(){
-    if(personalMovieDB.count < 10){
-        console.log("Ви подиивилися доволі мало фільмів");
-    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Ви класичний глядач");
-    } else if(personalMovieDB.count >= 30) {
-        console.log("Ви кіноман");
-    } else{
-        console.log("Сталася помилка");
+            if (genre === '' || genre == null){
+                console.log('Ви ввели некоректні дані або не ввели їх зовсім');
+                i--;
+            } else{
+                personalMovieDB.genres[i - 1] = genre;
+            }
+            
+            personalMovieDB.genres.forEach((item, i) => {
+               console.log(`Улюблений жанр ${i + 1} - це ${item}`);
+            });
+        }
     }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenders(){
-    for (let i = 1; i <=3; i++){
-        personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
-    }
-}
-
-writeYourGenders();
+};
